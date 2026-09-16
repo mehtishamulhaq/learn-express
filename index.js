00f391a7b59d7f8d4a81b946930039a3168d1210
Muhammad Ehtisham Ul Haq <muhammad.ehtisham@wearenova.co.uk>
2026-09-17 00:24:31 +0500

    new project with express setup and some routes

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import data from './data/MOCK_DATA.json' with { type: 'json' };
+
+const app = express();
+
+const PORT = 3000;
+
+// serving images
+app.use('/images', express.static('./public/images'));
+
+// GET
+app.get('/', (request, response) => {
+  response.json(data);
+});
+
+// POST
+app.post('/create', (request, response) => {
+  response.send('This is a post request at /create');
+});
+
+// PUT
+app.put('/update', (request, response) => {
+  response.send('This is a put request at /update');
+});
+
+// DELTETE
+app.delete('/delete', (request, response) => {
+  response.send('This is a delete request at /delete');
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}`);
+  console.log(data);
+});
